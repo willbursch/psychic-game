70ae1c2a71058c44e2b0f87475b7ac8d4e9de180
Will Bursch <will.bursch@gmail.com>
2018-08-18 22:07:38 -0400

    js scrap and build

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,198 @@
 
-//event listen for key to start game
-//random word generate from the array and display letter blanks. 
-//event listen for first letter guess
-//fill the word with any correct guess
-//document the incorrect guesses 
-//display winning image and add to win counter 
+//Arrays
+var wordArray = ["three", "foul", "lebron", "cavs", "lakers", "free throw", "dribble", "basketball",
+   "pass", "shoot", "score", "block", "steal", "turnover",]
 
-var wins = 0 
-//guesses left counter
-
-
+var gameStatus = false;
+var randomNumber = Math.floor(Math.random() * wordArray.length);
+var word = wordArray[randomNumber];
+var lettersRemaining = word.length;
+var answerArray = []; 
 
 
-// word and letter arrays 
-var alphabet = ["A", "B", "C", "D", "E", "F", "G",
- "H", "I", "J", "K", "L", "M", "N", "O", "P", 
- "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+//Use key events to listen for the letters.
+document.addEventListener("keyup", function(event){
+    //If gameStatus (or game round) has been initialized, then proceed to playing.
+    if(gameStatus) {
+        letterCheck(event);
+    } else {
+        //If gameStatus (or game round) has completed, re-initialize (or reset) the game.
+        init();
+    }
+});
 
-var words = ['BASKETBALL', 'FOUL', 'THREE', 'LEBRON', 
-'DUNK', 'PASS', 'SHOOT', 'FREE THROW', 'CHAMPION', 'FREE AGENT', 'LAKERS', 'CAVS']
+//Setup alphabet array for letter checking
+var alphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var randWord = words[Math.floor(Math.random() * words.length)];
+function letterCheck(guess) {
+    //If letter key is press, check if the letter pressed is in the answer.
+    if (alphabetArray.indexOf(guess.key) > -1) {
+        correctGuessCheck(guess);
+    }
+}
+
+//Check whether the guess is correct
+var winScore = 0;
+function correctGuessCheck(guess) {
+    if (word.indexOf(guess.key) > -1) {
+        //if guess is correct, run correctGuess function.
+        correctGuess(guess);
+    } else {
+        //If guess is incorrect, run incorrectGuess function.
+        incorrectGuess(guess);
+    }
+}
+
+function correctGuess(guess) {
+    if (answerArray.indexOf(guess.key.toUpperCase()) < 0) {
+        //If the correctGuess doesn't exist in the answerArray, run addCorrectLetter function.
+        addCorrectLetter(guess);
+    }
+}
+
+function addCorrectLetter(guess) {
+    for (var j = 0; j < word.length; j++) {
+        //If guess matches an existing letter in the answer.
+        if (guess.key === word[j]) {
+            //Push correct letter to answerArray as upperCase.
+            answerArray[j] = guess.key.toUpperCase();
+            displayCurrentWord();
+            //Reduce letters remaining for win by one.
+            lettersRemaining--;
+            //If letters left has reached 0, user wins. 
+            if (lettersRemaining === 0) {
+                //Add 1 to win score.
+                winScore++;
+                //Display new win score.
+                displayWins();
+                //Turn correct answer green.
+                addCorrect();
+                //display currentWord with new green font.
+                displayCurrentWord();
+            }
+        }
+    }
+}
+
+//Set up an incorrect answer array
+var incorrectGuessesMade = [];
+//Establish the number of guesses.
+var guessesLeft = 9;
+
+function incorrectGuess(guess) {
+    if (incorrectGuessesMade.indexOf(guess.key.toUpperCase()) < 0) {
+        //If the inCorrectGuess doesn't exist in the answerArray, run addIncorrectLetter function.
+        addIncorrectLetter(guess);
+    }
+}
+
+function addIncorrectLetter(guess) {
+    //Push incorrect guess into the incorrectGuessesMade array
+    incorrectGuessesMade.push(guess.key.toUpperCase());
+    //Inform user of incorrectGuessesMade
+    displayGuessesMade();
+    //Lower guessesLeft by 1
+    guessesLeft--;
+    //Inform user of guessesLeft
+    displayGuessesLeft();
+    if (guessesLeft === 0) {
+        //If guesses left reaches equals 0, then Game Over.
+        changeImage();
+        //Display corrent answer.
+        displayAnswer();
+    }
+}
+
+//Functions
+//Displays the number of wins user has obtains.
+function displayWins() {
+    var winsDisplay = document.querySelector("#winsDisplay");
+    winsDisplay.textContent = winScore;
+}
+
+//Displays the letters the user has guessed.
+function displayGuessesMade() {
+    var guessesMadeDisplay = document.querySelector("#guessesMadeDisplay");
+    guessesMadeDisplay.textContent = incorrectGuessesMade.join(", ");
+}
+
+//Displays how many user guesses are left.
+function displayGuessesLeft() {
+    var guessesLeftDisplay = document.querySelector("#guessesLeftDisplay");
+    guessesLeftDisplay.textContent = guessesLeft;
+}
+
+//Displays current solve status of answerArray.
+function displayCurrentWord() {
+    var currentWordDisplay = document.querySelector("#currentWordDisplay");
+    currentWordDisplay.innerHTML = answerArray.join(" ");
+}
 
 
-console.log(randWord)
-document.getElementById("test").innerHTML = 7
+//Reveals answer if user is unable to solve.
+function displayAnswer() {
+    var revealedAnswerDisplay = document.querySelector("#revealedAnswerDisplay");
+    revealedAnswerDisplay.textContent = word.toUpperCase();
+}
+
+//Turns current word green (to indicate correctness)
+function addCorrect() {
+    var currentWordDisplay = document.querySelector("#currentWordDisplay");
+    currentWordDisplay.classList.add('correct');
+}
+
+//Removes green color of current word (for re-initalizing purposes)
+function removeCorrect() {
+    var currentWordDisplay = document.querySelector("#currentWordDisplay");
+    currentWordDisplay.classList.remove('correct');
+}
 
 
-  // Show lives left and drop with incorrect guess
+function init() {
+    //Changes gameStatus to ready.
+    gameStatus = true;
+    
+    //Generate a new random number
+    randomNumber = Math.floor(Math.random() * wordArray.length);
+    
+    //Apply new randomNumber to obtain random word .
+    word = wordArray[randomNumber].word;
+
+    //Re-establish lettersRemaining (for win);
+    lettersRemaining = word.length;
+
+    //Re-establish answer array.
+     answerArray = []; 
+
+    //Convert word answer into an array.
+    for (var i = 0; i < word.length; i++) {
+        //If an answer has more than one word, use + as a separator. 
+        if (word[i] === "+") {
+            answerArray[i] = "&nbsp;";
+        } else {
+            //Replace word answer with "_"s
+            answerArray[i] = "_";
+        }
+    }
+
+    //Re-establish lettersRemaining (for win)
+    lettersRemaining = word.length;
+
+    //Re-establish guessesLeft for user.
+    guessesLeft = 9;
+    displayGuessesLeft()
+
+    //Re-establish guessesMade array.
+    incorrectGuessesMade = [];
+    displayGuessesMade()
+    
+    //Display current word.
+    displayCurrentWord();
 
 
+    //Empty revealedAnswer display if user was unsuccessful previously.
+    revealedAnswerDisplay.textContent = "";
 
+    //Remove greenColor from currentWord if user was successful previously.
+    removeCorrect();
+}
